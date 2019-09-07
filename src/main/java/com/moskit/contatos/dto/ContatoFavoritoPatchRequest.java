875c720d8becd8ef72409d0d0b3eda8233c5c1be
Leonardo Barbieri <leonardobdn@gmail.com>
2019-09-07 00:06:15 -0300

    Adiciona validação de nulo para o request de marcar/desmarcar contato como favorito

--- src/main/java/com/moskit/contatos/dto/ContatoFavoritoPatchRequest.java
+++ src/main/java/com/moskit/contatos/dto/ContatoFavoritoPatchRequest.java
@@ -1,7 +1,16 @@
 package com.moskit.contatos.dto;
 
+import javax.validation.constraints.NotNull;
+
 public class ContatoFavoritoPatchRequest {
+    @NotNull(message = "O campo favorito é obrigatório")
     private Boolean favorito;
+
+    public ContatoFavoritoPatchRequest(Boolean favorito) {
+        this.favorito = favorito;
+    }
+
+    public ContatoFavoritoPatchRequest() {}
 
     public Boolean getFavorito() {
         return favorito;
